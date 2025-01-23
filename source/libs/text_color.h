2d728ff756f2eace2ab19c395fa63c4406ba3dbc
lucasaamorim <lucasapolonio@hotmail.com>
2025-01-23 13:02:39 -0300

    Overhaul Text Formatting

--- source/libs/text_color.h
+++ source/libs/text_color.h
@@ -1,6 +1,4 @@
-#ifndef COLOR_H
-#define COLOR_H
-
+#pragma once
 /*!
  * Color code:
  * https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
@@ -8,27 +6,34 @@
  * How to use this function:
  * ```c++
  *      std::cout << "Esse texto aparece normal, "
- *                << Color::tcolor("texto em vermelho e negrito", Color::RED, Color::BOLD)
+ *                << Color::tcolor("texto em vermelho e negrito", Colors::RED, Modifiers::BOLD)
  *                << "\n";
  * ```
  */
 #include <sstream>
-
-using std::ostringstream;
-
 #include <string>
 
+using std::ostringstream;
 using std::string;
 
-#include <array>
+class TextFormat {
+public:
+  /// Returns a string with a colored message.
+  /*!
+   * @param msg Message to display.
+   * @param color Color code to apply to the message.
+   * @param modifier Modifier code to apply to the message.
+   * @return A string with the embedded color/modifier escape codes.
+   */
+  static std::string applyFormat(const std::string &msg, short color = Colors::WHITE, short modifier = Modifiers::REGULAR) {
+    ostringstream oss;
+    oss << "\33[" << modifier << ";" << color << "m" << msg << "\33[0m";
+    return oss.str();
+  }
+};
 
-using std::array;
-
-namespace Color {
-  // Alias
-  typedef short value_t;
-
-  /// Color attribute - colors
+/// @brief ANSI escape codes for text colors.
+struct Colors {
   static constexpr short BLACK{30};
   static constexpr short RED{31};
   static constexpr short GREEN{32};
@@ -45,29 +50,13 @@ namespace Color {
   static constexpr short BRIGHT_MAGENTA{95};
   static constexpr short BRIGHT_CYAN{96};
   static constexpr short BRIGHT_WHITE{97};
-  /// Color attributes - modifiers.
+};
+
+/// @brief ANSI escape codes for text modifiers. 
+struct Modifiers {
   static constexpr short REGULAR{0};
   static constexpr short BOLD{1};
   static constexpr short UNDERLINE{4};
   static constexpr short BLINK{5};
   static constexpr short REVERSE{7};
-
-  /// List of colors. You may wish to change color ordering.
-  static constexpr array<value_t, 14> color_list{
-      31, 32, 33, 34, 35, 36, 37,
-      91, 92, 93, 94, 95, 96, 97};
-
-  /// Returns a string with a colored message.
-  /*!
-   * @param msg Message to display.
-   * @param color Color code to apply to the message.
-   * @param modifier Modifier code to apply to the message.
-   * @return A string with the embedded color/modifier escape codes.
-   */
-  inline string tcolor(const string &msg, short color = Color::WHITE, short modifier = Color::REGULAR) {
-    ostringstream oss;
-    oss << "\33[" << modifier << ";" << color << "m" << msg << "\33[0m";
-    return oss.str();
-  }
-}
-#endif
+};
