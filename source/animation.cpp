5ecdbfb79662aabf615d6fd8c070c36842b6d130
lucasaamorim <lucasapolonio@hotmail.com>
2025-01-24 08:19:20 -0300

    Bar done and Frame almost done

--- source/animation.cpp
+++ source/animation.cpp
@@ -1,9 +1,15 @@
 #include "animation.h"
 
 void AnimationManager::PlayAnimation(int fps) {
+  // Save cursor position
+  cout << "\033[s";
   for (auto &frame : frames) {
-    renderer.clearScreen();
-    renderer.renderFrame(frame);
+    // Restore cursor position & clear screen
+    cout << "\033[u\033[J";
+    //FIXME: Replace 5 with the actual number of bars to render
+    if (categories.empty() or categories.size() > 15) frame.render(5);
+    else frame.render(categories,5);
+
     std::this_thread::sleep_for(std::chrono::milliseconds(1000 / fps));
   }
 }
--- source/barchart.h
+++ source/barchart.h
@@ -3,42 +3,44 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <map>
+#include <queue>
 #include "libs/text_color.h"
 
 using std::string;
 using std::vector;
 using std::cout;
-
 class Frame {
-  using color_t = short;
   class Bar {
     int length;
     int value;
-    color_t color;
     string label;
+    string category;
 
     public:
     inline bool operator<(const Bar &B) const { return this->value < B.value; }
     inline bool operator>(const Bar &B) const { return B < *this; }
     inline bool operator<=(const Bar &B) const { return !(*this > B); }
     inline bool operator>=(const Bar &B) const { return !(*this < B); }
-    void render();
+    void render(color_t color) const;
     void setLength(const int &length) { this->length = length; }
     void setValue(const int &value) { this->value = value; }
-    void setColor(const color_t &color) { this->color = color; }
+    void setCategory(const string &category) { this->category = category; }
+    void setLabel(const string &label) { this->label = label; }
+    string getCategory() const { return category; }
   };
 
-  vector<Bar> bars;
+  std::priority_queue<Bar> bars; //Bars in descending order
   string title;
   string x_label;
-  string y_label;
   string timestamp;
+  string source;
 
   public:
-  void render();
-  void addBar(const Bar &bar) { bars.emplace_back(bar); }
+  void render(int n_bars); //Has > 15 categories or none
+  void render(std::map<string,color_t> categories, int n_bars); //Has between 1 and 15 categories
+  void addBar(const Bar &bar) { bars.push(bar); }
   void setTitle(const string &title) { this->title = title; }
   void setXLabel(const string &x_label) { this->x_label = x_label; }
-  void setYLabel(const string &y_label) { this->y_label = y_label; }
   void setTimestamp(const string &timestamp) { this->timestamp = timestamp; }
 };
